bf555cd43cdeb9fd2ec539b38cecd70ed239fc50
Rohit Tewari <rtewari056@gmail.com>
2022-09-27 15:47:27 +0530

    Added Higher Order Component for Page Scroll Animation

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-import { AppWrap } from "../../wrapper";
+import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./About.scss";
 
@@ -49,4 +49,8 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, "about");
+export default AppWrap(
+  MotionWrap(About, "app__about"),
+  "about",
+  "app__whitebg"
+);
